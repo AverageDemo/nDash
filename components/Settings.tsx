eedef8dc70eb623efc0d2e741a18b4d4c5b3cfb0
Average Demo <46252711+AverageDemo@users.noreply.github.com>
2021-08-02 03:53:15 -0400

    Ability to switch your temp units

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,10 +1,27 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from 'react';
+import { FormEvent, Fragment, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { CheckIcon, CogIcon } from '@heroicons/react/outline';
+import { CogIcon } from '@heroicons/react/outline';
+import { useRouter } from 'next/router';
 
 export default function Settings() {
+  const router = useRouter();
   const [open, setOpen] = useState(false);
+  const [values, setValues] = useState({
+    unit: localStorage.getItem('unit') ?? 2,
+  });
+
+  const handleInputChange = (e: React.FormEvent) => {
+    const { name, value } = e.target as HTMLInputElement;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    localStorage.setItem('unit', String(values.unit));
+    router.reload();
+  };
 
   return (
     <>
@@ -42,27 +59,40 @@ export default function Settings() {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
-                <div>
-                  <div className="mt-3 text-center sm:mt-5">
-                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                      Settings
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.
-                      </p>
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <div className="text-center">
+                      <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900 pb-4">
+                        Settings
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <label htmlFor="unit" className="block text-sm font-medium text-gray-700 text-left">
+                          Unit
+                        </label>
+                        <select
+                          id="unit"
+                          name="unit"
+                          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                          defaultValue={values.unit}
+                          onChange={handleInputChange}
+                        >
+                          <option value="0">Celsius</option>
+                          <option value="1">Fahrenheit</option>
+                          <option value="2">Kelvin</option>
+                        </select>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="mt-5 sm:mt-6">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-                    onClick={() => setOpen(false)}
-                  >
-                    Save
-                  </button>
-                </div>
+                  <div className="mt-5 sm:mt-6">
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                      onClick={() => setOpen(false)}
+                    >
+                      Save
+                    </button>
+                  </div>
+                </form>
               </div>
             </Transition.Child>
           </div>
