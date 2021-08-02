5da550d71cddba524f8cc90171f492495b53af4e
Average Demo <46252711+AverageDemo@users.noreply.github.com>
2021-08-02 03:24:44 -0400

    Update BWelcome.tsx

--- components/blocks/BWelcome.tsx
+++ components/blocks/BWelcome.tsx
@@ -6,12 +6,13 @@ export default function BWelcome({ user }: Props) {
   return (
     <div className="my-1 px-1 w-1/4 overflow-hidden">
       <BCard>
-        <div className="p-2">
-          <div className="text-6xl pb-6 font-thin">
-            Hello<p>{user}</p>
-          </div>
-          <div className="my-auto py-2 text-blue-700 dark:text-blue-200">
-            Today is <span className="text-gray-900 dark:text-gray-50">{moment().format('dddd, MMMM Do YYYY')}</span>
+        <div className="p-2 text-center">
+          <div className="text-6xl pb-6 font-thin">Hello {user}</div>
+          <div className="mt-4">
+            <div className="text-4xl font-thin text-blue-700 dark:text-blue-200">{moment().format('hh:mm A')}</div>
+            <div className="my-auto mt-2 pb-3">
+              <span className="text-gray-900 dark:text-gray-50">{moment().format('dddd, MMMM Do YYYY')} </span>
+            </div>
           </div>
         </div>
       </BCard>
